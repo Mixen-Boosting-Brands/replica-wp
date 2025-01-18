b63b2489d2f8674f639e1978ba7d47acf4f1f45a
Luis Carlos Pando <hey@luiscarlospando.com>
2025-01-17 20:35:22 -0600

    Mejorar apariencia del thumbnail

--- single.php
+++ single.php
@@ -11,7 +11,7 @@
                     <article>
                         <div class="thumbnail text-center w-100 mb-4">
                             <?php if (has_post_thumbnail()): ?>
-                                <?php the_post_thumbnail("medium", [
+                                <?php the_post_thumbnail("large", [
                                     "class" => "img-fluid rounded",
                                 ]); ?>
                             <?php endif; ?>
