95c7eeafad3aeac012984afc6659ecf3d9d2aca3
Luis Carlos Pando <hey@luiscarlospando.com>
2025-01-17 20:33:13 -0600

    Mejorar apariencia del thumbnail

--- single.php
+++ single.php
@@ -9,11 +9,13 @@
             <div class="row">
                 <div class="col-12">
                     <article>
-                        <?php if (has_post_thumbnail()): ?>
-                            <?php the_post_thumbnail("full", [
-                                "class" => "img-fluid mb-4",
-                            ]); ?>
-                        <?php endif; ?>
+                        <div class="thumbnail text-center w-100 mb-4">
+                            <?php if (has_post_thumbnail()): ?>
+                                <?php the_post_thumbnail("medium", [
+                                    "class" => "img-fluid rounded",
+                                ]); ?>
+                            <?php endif; ?>
+                        </div>
 
                         <h1>
                             <?php the_title(); ?>
