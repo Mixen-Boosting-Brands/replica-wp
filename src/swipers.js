e9aea0541301e785271397f854e8fb4b05569a45
Luis Carlos Pando <hey@luiscarlospando.com>
2025-01-31 14:45:36 -0600

    Ajustar cómo se despliegan elementos del blog

--- src/swipers.js
+++ src/swipers.js
@@ -170,15 +170,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
         breakpoints: {
             576: {
-                slidesPerView: 3,
+                slidesPerView: 1,
                 spaceBetween: 40,
             },
             768: {
-                slidesPerView: 4,
+                slidesPerView: 2,
                 spaceBetween: 40,
             },
             992: {
-                slidesPerView: 5,
+                slidesPerView: 2.5,
                 spaceBetween: 40,
                 centeredSlides: false,
             },
