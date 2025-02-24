ca59dae331729285fff8b01c6f1a16360692786c
Luis Carlos Pando <hey@luiscarlospando.com>
2025-02-24 16:05:07 -0600

    Ajustes del cliente

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ get_header();
                     data-aos="fade-in"
                     data-aos-duration="1000"
                     data-aos-delay="900"
-                    data-aos-offset="400"
+                    data-aos-offset="0"
                 />
             </div>
         </div>
